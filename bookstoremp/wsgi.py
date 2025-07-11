369837aaa9ba4fbc7d720d9e72ffb4f46e7abc7f
MichelPianaro <teconhecer27@gmail.com>
2025-07-10 21:49:09 -0300

    atual

--- bookstoremp/wsgi.py
+++ bookstoremp/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstoremp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstore.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), "staticfiles"))
