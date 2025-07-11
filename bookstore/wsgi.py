51df8ddaedc111a05d28a5a22fc51f56ef502258
MichelPianaro <teconhecer27@gmail.com>
2025-07-10 21:12:07 -0300

    atualizando

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -1,17 +1,8 @@
 import os
-import sys
-
-# Caminho para sua pasta do projeto
-project_path = '/home/Michaelmp/bookstoremp'
-if project_path not in sys.path:
-    sys.path.insert(0, project_path)
-
-# Caminho do virtualenv
-activate_env = '/home/Michaelmp/.virtualenvs/bookstoremp/bin/activate_this.py'
-with open(activate_env) as file_:
-    exec(file_.read(), dict(__file__=activate_env))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstoremp.settings')
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstore.settings")
+
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), "staticfiles"))
