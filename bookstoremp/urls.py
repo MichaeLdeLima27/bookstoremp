f5ce3660d3d3ae3ddfc5044ec9c769d41fb00084
MichelPianaro <teconhecer27@gmail.com>
2025-07-11 22:19:44 -0300

    ffinalizando finalmente

--- bookstoremp/urls.py
+++ bookstoremp/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for bookstore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from bookstoremp import views
 from django.contrib import admin
 from django.urls import include, path, re_path
@@ -28,4 +11,5 @@ urlpatterns = [
     path("api-token-auth/", obtain_auth_token, name="api-token-auth"),
     path("update_server/", views.update, name="update"),
     path("hello/", views.hello_world, name="hello_world"),
+    path("", views.home, name="home"),  # Rota da home
 ]
--- bookstoremp/views.py
+++ bookstoremp/views.py
@@ -4,24 +4,20 @@ from django.views.decorators.csrf import csrf_exempt
 
 import git
 
-
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
         repo = git.Repo('/home/Michaelmp/bookstoremp')
         origin = repo.remotes.origin
-
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
     else:
         return HttpResponse("Couldn't update the code on PythonAnywhere")
 
-
 def hello_world(request):
-  template = loader.get_template('hello_world.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('hello_world.html')
+    return HttpResponse(template.render({}, request))
+
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render({}, request))
