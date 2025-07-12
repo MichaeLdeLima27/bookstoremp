c4467492508894dcf0852ab74ca7872b36a4f595
MichelPianaro <teconhecer27@gmail.com>
2025-07-11 22:30:55 -0300

    final

--- bookstoremp/urls.py
+++ bookstoremp/urls.py
@@ -1,15 +1,11 @@
-from bookstoremp import views
 from django.contrib import admin
-from django.urls import include, path, re_path
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
+from bookstoremp import views
 
 urlpatterns = [
-    path("__debug__", include("debug_toolbar.urls")),
-    path("admin/", admin.site.urls),
-    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
-    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
-    path("api-token-auth/", obtain_auth_token, name="api-token-auth"),
-    path("update_server/", views.update, name="update"),
-    path("hello/", views.hello_world, name="hello_world"),
-    path("", views.home, name="home"),  # Rota da home
+    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('hello/', views.hello_world, name='hello_world'),
+    path('update_server/', views.update, name='update'),
+    path('api-token-auth/', include('rest_framework.urls')),
 ]
