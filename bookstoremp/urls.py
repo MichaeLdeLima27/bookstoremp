8ba783ce4cae56d977f5365b72002696b77c0d06
MichelPianaro <teconhecer27@gmail.com>
2025-07-15 23:01:22 -0300

    finalmp2

--- bookstoremp/urls.py
+++ bookstoremp/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from bookstoremp import views
-from bookstoremp.views import ProductDetailAPIView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('hello/', views.hello_world, name='hello_world'),
     path('update_server/', views.update, name='update'),
-    path('api-token-auth/', include('rest_framework.urls')),
-    path('api/product/<int:pk>/', ProductDetailAPIView.as_view(), name='api-product-detail'),  # API REST para detalhes do produto
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),  # Página de detalhes do produto (HTML)
+    path('api-token-auth/', include('rest_framework.urls')),  # login/logout DRF browsable API
+    path('api/products/', views.ProductListCreateAPIView.as_view(), name='api-product-list-create'),  # Listar e criar produtos
+    path('api/products/<int:pk>/', views.ProductDetailAPIView.as_view(), name='api-product-detail'),  # Detalhe produto API
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),  # Página HTML de produto
 ]
--- bookstoremp/views.py
+++ bookstoremp/views.py
@@ -2,28 +2,48 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
-from product.models import Product
 import git
 
+from rest_framework.generics import RetrieveAPIView, ListCreateAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from product.models.product import Product
+from .serializers import ProductSerializer
+
+
 @csrf_exempt
+@api_view(['POST'])
 def update(request):
-    if request.method == "POST":
-        repo = git.Repo('/home/Michaelmp/bookstoremp')
-        origin = repo.remotes.origin
-        origin.pull()
-        return HttpResponse("Updated code on PythonAnywhere")
-    return HttpResponse("Couldn't update the code on PythonAnywhere")
+    repo = git.Repo('/home/Michaelmp/bookstoremp')
+    origin = repo.remotes.origin
+    origin.pull()
+    return Response({"detail": "Updated code on PythonAnywhere"}, status=status.HTTP_200_OK)
+
 
 def hello_world(request):
     template = loader.get_template('hello_world.html')
     return HttpResponse(template.render({}, request))
 
+
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render({}, request))
+
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     template = loader.get_template('product_detail.html')
     context = {'product': product}
     return HttpResponse(template.render(context, request))
+
+
+class ProductDetailAPIView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductListCreateAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
